937b7619803ccfd9d9ed0f2a88397acb46f143b7
shibu <shibu_st@yahoo.com>
2020-02-13 21:13:14 +0530

    Fallback Policy

--- Controllers/WeatherAggregatorController.cs
+++ Controllers/WeatherAggregatorController.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Polly;
+using Polly.Fallback;
 using Polly.Retry;
 
 namespace ResilientWebSvc.Controllers
@@ -19,28 +23,50 @@ public class WeatherAggregatorController:ControllerBase{
 
         private readonly ILogger<WeatherAggregatorController> _logger;
         private readonly AsyncRetryPolicy<HttpResponseMessage> _httpRetryPolicy;
+        private readonly AsyncFallbackPolicy<HttpResponseMessage> _httpRequestFallbackPolicy;
+        
+        private HttpClient _httpClient;
 
+#region Fallback
         public WeatherAggregatorController(ILogger<WeatherAggregatorController> logger)
         {
             _logger = logger;
+            var defaultResult = new List<WeatherForecast>(){};
 
-            //immediate retry
-            //_httpRetryPolicy = Policy.HandleResult<HttpResponseMessage>( r => !r.IsSuccessStatusCode)
-            //    .RetryAsync(3);
+            
 
-            //wait and retry
-            _httpRetryPolicy = Policy.HandleResult<HttpResponseMessage>( r => !r.IsSuccessStatusCode)
-                .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2,retryAttempt) / 2));
+            //retry with fallback 
+            _httpRetryPolicy = Policy.HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode).RetryAsync(3);
+
+            _httpRequestFallbackPolicy = 
+                Policy.HandleResult<HttpResponseMessage>(r => r.StatusCode == HttpStatusCode.InternalServerError)
+                .FallbackAsync(
+                    new HttpResponseMessage(HttpStatusCode.OK){                        
+
+                        Content = new ObjectContent(defaultResult.GetType(),defaultResult,new JsonMediaTypeFormatter() )
+
+                    }
+
+                );
+
+
+
         }
 
+
+         //retry with Fallback
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            var httpClient = GetHttpClient();
+             _httpClient = GetHttpClient();
             var requestEndPoint = $"/WeatherForecast";
 
-            //var response = await httpClient.GetAsync(requestEndPoint);
-            var response = await _httpRetryPolicy.ExecuteAsync(() => httpClient.GetAsync(requestEndPoint));
+            
+            var response = await _httpRequestFallbackPolicy.ExecuteAsync( () =>
+            
+                _httpRetryPolicy.ExecuteAsync(() => _httpClient.GetAsync(requestEndPoint))
+
+            );
 
             if(response.IsSuccessStatusCode){
                 IEnumerable<WeatherForecast> result = JsonConvert.DeserializeObject<IEnumerable<WeatherForecast>>(
@@ -53,6 +79,131 @@ public class WeatherAggregatorController:ControllerBase{
             return StatusCode((int) response.StatusCode,response.Content.ReadAsStringAsync());
 
         }
+
+#endregion
+
+
+#region UnAuthorized Retry
+
+       /* //retry for Auth
+        public WeatherAggregatorController(ILogger<WeatherAggregatorController> logger)
+        {
+            _logger = logger;
+            //unauthorized retry
+            _httpRetryPolicy = Policy.HandleResult<HttpResponseMessage>( r => !r.IsSuccessStatusCode)
+                .RetryAsync(3, onRetry: (httpResponseMessage,i) =>
+                {
+                    if (httpResponseMessage.Result.StatusCode == HttpStatusCode.Unauthorized)
+                        PerformReAuthorization();                    
+                        
+                    }
+                ); 
+
+            
+
+
+
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+             _httpClient = GetHttpClient("BadAuthCode");
+            var requestEndPoint = $"/WeatherForecast";
+
+            //var response = await httpClient.GetAsync(requestEndPoint);
+            var response = await _httpRetryPolicy.ExecuteAsync(() => _httpClient.GetAsync(requestEndPoint));
+
+            if(response.IsSuccessStatusCode){
+                IEnumerable<WeatherForecast> result = JsonConvert.DeserializeObject<IEnumerable<WeatherForecast>>(
+                    await response.Content.ReadAsStringAsync()
+                );
+
+                return Ok(result);
+
+            }
+            return StatusCode((int) response.StatusCode,response.Content.ReadAsStringAsync());
+
+        }
+
+        private void PerformReAuthorization()
+        {
+          _httpClient = GetHttpClient("GoodAuthCode");
+        }
+
+        private HttpClient GetHttpClient(string authCode)
+        {
+            //ByPass SSL validation checks
+            var handler = new HttpClientHandler();
+            handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+            handler.ServerCertificateCustomValidationCallback = 
+            (httpRequestMessage,cert,certChain,policyErrors) => {
+                return true;
+            };
+
+            var cookieContainer = new CookieContainer();
+            cookieContainer.Add(new Uri("http://localhost") , new Cookie("Auth", authCode));
+            handler.CookieContainer = cookieContainer;
+
+
+            var httpClient = new HttpClient(handler);
+            httpClient.BaseAddress = new Uri("https://localhost:5001/");
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return httpClient;
+        }
+
+ */
+
+ #endregion
+        
+        
+#region BasicRetry&wait 
+   /*
+        
+        //  //basic retry and wait
+
+        public WeatherAggregatorController(ILogger<WeatherAggregatorController> logger)
+        {
+            _logger = logger;
+
+            //immediate retry
+            //_httpRetryPolicy = Policy.HandleResult<HttpResponseMessage>( r => !r.IsSuccessStatusCode)
+            //    .RetryAsync(3);
+
+            wait and retry
+            _httpRetryPolicy = Policy.HandleResult<HttpResponseMessage>( r => !r.IsSuccessStatusCode)
+                .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2,retryAttempt) / 2));
+
+
+
+        }
+
+
+        // [HttpGet]
+        // public async Task<IActionResult> Get()
+        // {
+        //     var httpClient = GetHttpClient();
+        //     var requestEndPoint = $"/WeatherForecast";
+
+        //     //var response = await httpClient.GetAsync(requestEndPoint);
+        //     var response = await _httpRetryPolicy.ExecuteAsync(() => httpClient.GetAsync(requestEndPoint));
+
+        //     if(response.IsSuccessStatusCode){
+        //         IEnumerable<WeatherForecast> result = JsonConvert.DeserializeObject<IEnumerable<WeatherForecast>>(
+        //             await response.Content.ReadAsStringAsync()
+        //         );
+
+        //         return Ok(result);
+
+        //     }
+        //     return StatusCode((int) response.StatusCode,response.Content.ReadAsStringAsync());
+
+        // }
+
+        */
+    #endregion
 
         private HttpClient GetHttpClient(){
 
@@ -72,6 +223,9 @@ public class WeatherAggregatorController:ControllerBase{
             return httpClient;
 
         }
+
+
+        
 
 
 }
--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -25,38 +25,80 @@ namespace ResilientWebSvc.Controllers
         {
             _logger = logger;
         }
+#region Fallback
 
+     
         [HttpGet]
         public async Task<IActionResult>  Get()
         {
-            _requestCount++;
             
-            if(_requestCount % 4 == 0){
-                var rng = new Random();
-
-                var resultTask = Task.Run(() => {
-                    return
-                        Enumerable.Range(1, 5).Select(index => new WeatherForecast
-                        {
-                            Date = DateTime.Now.AddDays(index),
-                            TemperatureC = rng.Next(-20, 55),
-                            Summary = Summaries[rng.Next(Summaries.Length)]
-                        })
-                        .ToArray();
-                });
-
-                var response = await resultTask;
-                return Ok(response);
-
-            }
-
-            return StatusCode((int) HttpStatusCode.InternalServerError,"Error");
-
-            
-
-             
-
+            return await Task.Run(() => StatusCode((int) HttpStatusCode.InternalServerError,"Error"));
            
         }
+#endregion
+        
+    #region UnAuthorized Retry
+
+        // //AuthFlow
+        // [HttpGet]
+        // public async Task<IActionResult>  Get()
+        // {
+            
+        //     string authCode = Request.Cookies["Auth"];
+            
+        //     if(authCode == "GoodAuthCode"){
+        //         var rng = new Random();
+
+        //         var resultTask = Task.Run(() => {
+        //             return
+        //                 Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //                 {
+        //                     Date = DateTime.Now.AddDays(index),
+        //                     TemperatureC = rng.Next(-20, 55),
+        //                     Summary = Summaries[rng.Next(Summaries.Length)]
+        //                 })
+        //                 .ToArray();
+        //         });
+
+        //         var response = await resultTask;
+        //         return Ok(response);
+
+        //     }
+
+        //     return StatusCode((int) HttpStatusCode.Unauthorized,"Not Authorized");
+        // }
+
+    #endregion
+       
+    #region BasicRetry&wait
+     //// basic flow
+        // [HttpGet]
+        // public async Task<IActionResult>  Get()
+        // {
+        //     _requestCount++;
+            
+        //     if(_requestCount % 4 == 0){
+        //         var rng = new Random();
+
+        //         var resultTask = Task.Run(() => {
+        //             return
+        //                 Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //                 {
+        //                     Date = DateTime.Now.AddDays(index),
+        //                     TemperatureC = rng.Next(-20, 55),
+        //                     Summary = Summaries[rng.Next(Summaries.Length)]
+        //                 })
+        //                 .ToArray();
+        //         });
+
+        //         var response = await resultTask;
+        //         return Ok(response);
+
+        //     }
+
+        //     return StatusCode((int) HttpStatusCode.InternalServerError,"Error");
+           
+        // }
+    #endregion
     }
 }
